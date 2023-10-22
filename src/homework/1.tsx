7e1f216efd3715f7ff353a3189efe031a9d907de
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-10-22 23:46:21 +0300

    add

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,29 +1,51 @@
-import React, { useEffect, useRef } from 'react';
+import { type } from "os";
+import React, {
+  useEffect,
+  useRef,
+} from "react";
 
 // Опишіть Props
-export function Observer({ children, onContentEndVisible }: Props) {
+type Props = {
+  children: React.ReactNode;
+  onContentEndVisible(): void;
+};
+export function Observer({
+  children,
+  onContentEndVisible,
+}: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef =
+    useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0,
-      root: null,
-    };
+    const options: IntersectionObserverInit =
+      {
+        rootMargin: "0px",
+        threshold: 1.0,
+        root: null,
+      };
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-          onContentEndVisible();
-          observer.disconnect();
-        }
-      });
-    }, options);
+    const observer =
+      new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (
+              entry.intersectionRatio >
+              0
+            ) {
+              onContentEndVisible();
+              observer.disconnect();
+            }
+          });
+        },
+        options
+      );
 
     if (endContentRef.current) {
-      observer.observe(endContentRef.current);
+      observer.observe(
+        endContentRef.current
+      );
     }
 
     return () => {
